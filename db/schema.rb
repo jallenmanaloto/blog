73b888b8304e46753aabc652444f34feaf5ff6f7
allen <jallen.manaloto@gmail.com>
2021-11-09 21:14:42 +0800

    Lesson: updating active records

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_06_035128) do
+ActiveRecord::Schema.define(version: 2021_11_09_115040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "games", force: :cascade do |t|
     t.string "name"
