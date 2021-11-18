67946c51f74b58875085e88a0dd1c7da9ace1ce1
jallenmanaloto <jallen.manaloto@gmail.com>
2021-11-18 18:58:43 +0800

    installed rspec

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+
     def index
         @articles = Article.all
     end
@@ -7,12 +8,11 @@ class ArticlesController < ApplicationController
     end
 
     def new
+        @article = Article.new
     end
 
     def create
-        @article = Article.new
-        @article.name = params[:name]
-        @article.body = params[:body]
+        @article = Article.new(article_params)
 
         if @article.save
             redirect_to articles_path
@@ -28,5 +28,11 @@ class ArticlesController < ApplicationController
     end
 
     def delete
+    end
+
+    private
+
+    def article_params
+        params.require(:article).permit(:name, :body)
     end
 end
