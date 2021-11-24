ac5e82483701e275c41c9946c447ade84a951305
jallenmanaloto <jallen.manaloto@gmail.com>
2021-11-24 10:56:41 +0800

    fixed routing to category

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :categories
-  resources :games
+  # resources :games
 
   # get '/articles' => 'articles#index'
   # get '/articles/new' => 'articles#new', as: 'new_article'
   # post 'articles' => 'articles#create', as: 'create_article'
-  get '/' => 'games#index'
+  get '/' => 'categories#index'
   # get '/categories/new' => 'categories#new', as: 'new_category'
   # get '/categories/:id' => 'categories#show', as: 'show_category'
 end
