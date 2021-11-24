11b69589cac64a93ee0d477b181988178bd9900e
jallenmanaloto <jallen.manaloto@gmail.com>
2021-11-24 10:38:38 +0800

    redo routing for producttion page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # get '/articles' => 'articles#index'
   # get '/articles/new' => 'articles#new', as: 'new_article'
   # post 'articles' => 'articles#create', as: 'create_article'
-  get '/' => 'categories#index'
+  # get '/' => 'categories#index'
   # get '/categories/new' => 'categories#new', as: 'new_category'
   # get '/categories/:id' => 'categories#show', as: 'show_category'
 end
