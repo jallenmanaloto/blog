c241397af2990a2d0935e9f04c209f9eaa345385
jallenmanaloto <jallen.manaloto@gmail.com>
2021-11-24 10:35:24 +0800

    fixed root routing of page

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   resources :categories
   resources :games
 
-  get '/articles' => 'articles#index'
-  get '/articles/new' => 'articles#new', as: 'new_article'
-  post 'articles' => 'articles#create', as: 'create_article'
-  # get '/categories' => 'categories#index'
+  # get '/articles' => 'articles#index'
+  # get '/articles/new' => 'articles#new', as: 'new_article'
+  # post 'articles' => 'articles#create', as: 'create_article'
+  get '/' => 'categories#index'
   # get '/categories/new' => 'categories#new', as: 'new_category'
   # get '/categories/:id' => 'categories#show', as: 'show_category'
 end
