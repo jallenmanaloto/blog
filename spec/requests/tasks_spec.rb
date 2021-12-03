dc351fbf7a1428ee5eda384cad3f5a3cb122e9be
jallenmanaloto <jallen.manaloto@gmail.com>
2021-12-03 14:55:24 +0800

    finished rspec test for Tasks index

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -12,119 +12,109 @@
 # of tools you can use to make these specs even more expressive, but we're
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
-RSpec.describe "/tasks", type: :request do
+RSpec.describe "Tasks", type: :request do
   
-  # Task. As you add validations to Task, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
-  describe "GET /index" do
+   describe "GET index of tasks" do
     it "renders a successful response" do
-      Task.create! valid_attributes
-      get tasks_url
-      expect(response).to be_successful
+      category = Category.create(name: 'Test')
+      get category_tasks_path(category.id)
+      expect(response).to have_http_status(200)
     end
   end
 
-  describe "GET /show" do
-    it "renders a successful response" do
-      task = Task.create! valid_attributes
-      get task_url(task)
-      expect(response).to be_successful
-    end
-  end
+  # describe "GET /show" do
+  #   it "renders a successful response" do
+  #     task = Task.create! valid_attributes
+  #     get task_url(task)
+  #     expect(response).to be_successful
+  #   end
+  # end
 
-  describe "GET /new" do
-    it "renders a successful response" do
-      get new_task_url
-      expect(response).to be_successful
-    end
-  end
+  # describe "GET /new" do
+  #   it "renders a successful response" do
+  #     get new_task_url
+  #     expect(response).to be_successful
+  #   end
+  # end
 
-  describe "GET /edit" do
-    it "render a successful response" do
-      task = Task.create! valid_attributes
-      get edit_task_url(task)
-      expect(response).to be_successful
-    end
-  end
+  # describe "GET /edit" do
+  #   it "render a successful response" do
+  #     task = Task.create! valid_attributes
+  #     get edit_task_url(task)
+  #     expect(response).to be_successful
+  #   end
+  # end
 
-  describe "POST /create" do
-    context "with valid parameters" do
-      it "creates a new Task" do
-        expect {
-          post tasks_url, params: { task: valid_attributes }
-        }.to change(Task, :count).by(1)
-      end
+  # describe "POST /create" do
+  #   context "with valid parameters" do
+  #     it "creates a new Task" do
+  #       expect {
+  #         post tasks_url, params: { task: valid_attributes }
+  #       }.to change(Task, :count).by(1)
+  #     end
 
-      it "redirects to the created task" do
-        post tasks_url, params: { task: valid_attributes }
-        expect(response).to redirect_to(task_url(Task.last))
-      end
-    end
+  #     it "redirects to the created task" do
+  #       post tasks_url, params: { task: valid_attributes }
+  #       expect(response).to redirect_to(task_url(Task.last))
+  #     end
+  #   end
 
-    context "with invalid parameters" do
-      it "does not create a new Task" do
-        expect {
-          post tasks_url, params: { task: invalid_attributes }
-        }.to change(Task, :count).by(0)
-      end
+  #   context "with invalid parameters" do
+  #     it "does not create a new Task" do
+  #       expect {
+  #         post tasks_url, params: { task: invalid_attributes }
+  #       }.to change(Task, :count).by(0)
+  #     end
 
-      it "renders a successful response (i.e. to display the 'new' template)" do
-        post tasks_url, params: { task: invalid_attributes }
-        expect(response).to be_successful
-      end
-    end
-  end
+  #     it "renders a successful response (i.e. to display the 'new' template)" do
+  #       post tasks_url, params: { task: invalid_attributes }
+  #       expect(response).to be_successful
+  #     end
+  #   end
+  # end
 
-  describe "PATCH /update" do
-    context "with valid parameters" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
+  # describe "PATCH /update" do
+  #   context "with valid parameters" do
+  #     let(:new_attributes) {
+  #       skip("Add a hash of attributes valid for your model")
+  #     }
 
-      it "updates the requested task" do
-        task = Task.create! valid_attributes
-        patch task_url(task), params: { task: new_attributes }
-        task.reload
-        skip("Add assertions for updated state")
-      end
+  #     it "updates the requested task" do
+  #       task = Task.create! valid_attributes
+  #       patch task_url(task), params: { task: new_attributes }
+  #       task.reload
+  #       skip("Add assertions for updated state")
+  #     end
 
-      it "redirects to the task" do
-        task = Task.create! valid_attributes
-        patch task_url(task), params: { task: new_attributes }
-        task.reload
-        expect(response).to redirect_to(task_url(task))
-      end
-    end
+  #     it "redirects to the task" do
+  #       task = Task.create! valid_attributes
+  #       patch task_url(task), params: { task: new_attributes }
+  #       task.reload
+  #       expect(response).to redirect_to(task_url(task))
+  #     end
+  #   end
 
-    context "with invalid parameters" do
-      it "renders a successful response (i.e. to display the 'edit' template)" do
-        task = Task.create! valid_attributes
-        patch task_url(task), params: { task: invalid_attributes }
-        expect(response).to be_successful
-      end
-    end
-  end
+  #   context "with invalid parameters" do
+  #     it "renders a successful response (i.e. to display the 'edit' template)" do
+  #       task = Task.create! valid_attributes
+  #       patch task_url(task), params: { task: invalid_attributes }
+  #       expect(response).to be_successful
+  #     end
+  #   end
+  # end
 
-  describe "DELETE /destroy" do
-    it "destroys the requested task" do
-      task = Task.create! valid_attributes
-      expect {
-        delete task_url(task)
-      }.to change(Task, :count).by(-1)
-    end
+  # describe "DELETE /destroy" do
+  #   it "destroys the requested task" do
+  #     task = Task.create! valid_attributes
+  #     expect {
+  #       delete task_url(task)
+  #     }.to change(Task, :count).by(-1)
+  #   end
 
-    it "redirects to the tasks list" do
-      task = Task.create! valid_attributes
-      delete task_url(task)
-      expect(response).to redirect_to(tasks_url)
-    end
-  end
+  #   it "redirects to the tasks list" do
+  #     task = Task.create! valid_attributes
+  #     delete task_url(task)
+  #     expect(response).to redirect_to(tasks_url)
+  #   end
+  # end
 end
